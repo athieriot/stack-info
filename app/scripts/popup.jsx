0c255ca4970414ceecfda7464bbab520f8f8f9a4
Aurélien Thieriot <a.thieriot@gmail.com>
2014-02-05 21:28:30 +0000

    Link open a new tab

--- app/scripts/popup.jsx
+++ app/scripts/popup.jsx
@@ -58,6 +58,9 @@ var StackUser = React.createClass({
       this.setState({user: result.user});
     }.bind(this));
   },
+  openTab: function(event) {
+    chrome.tabs.create({ url: event.target.parentElement.href });
+  },
   render: function() {
     if (this.state.user) {
       return (
@@ -65,15 +68,15 @@ var StackUser = React.createClass({
           <div className="black-band"></div>
           <div className="container inner-shadow">
             <div className="media">
-              <a className="pull-left" href={this.state.user.website_url}>
+              <a className="pull-left" href={this.state.user.website_url} onClick={this.openTab}>
                 <img className="media-object thumbnail" src={this.state.user.profile_image} title={decodeHtmlEntity(this.state.user.display_name)} height="92px"/>
               </a>
               <div className="media-body">
-                <h4 className="media-heading">{decodeHtmlEntity(this.state.user.display_name)}</h4>
+                <a href={this.state.user.link} onClick={this.openTab}><h4 className="media-heading">{decodeHtmlEntity(this.state.user.display_name)}</h4></a>
                 {this.state.user.location} <br /><br />
-                <img src="../images/gold_medal.png" height="16px"/> {this.state.user.badge_counts.gold}
-                <img src="../images/silver_medal.png" height="16px"/> {this.state.user.badge_counts.silver}
-                <img src="../images/bronze_medal.png" height="16px"/> {this.state.user.badge_counts.bronze}
+                <span className="badge-number"><img src="../images/gold_medal.png" height="16px"/> {this.state.user.badge_counts.gold}</span>
+                <span className="badge-number"><img src="../images/silver_medal.png" height="16px"/> {this.state.user.badge_counts.silver}</span>
+                <span className="badge-number"><img src="../images/bronze_medal.png" height="16px"/> {this.state.user.badge_counts.bronze}</span>
               </div>
             </div>
           <RewardProgress />
